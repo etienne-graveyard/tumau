ef87eb21b7a7346fa26d57e1a772abcee568cb01
Etienne Dldc <e.deladonchamps@gmail.com>
2019-11-05 19:41:24 +0100

    Add type on JsonResponse.with

--- packages/tumau-json/src/JsonResponse.ts
+++ packages/tumau-json/src/JsonResponse.ts
@@ -1,16 +1,16 @@
 import { TumauResponse, HttpStatusCode, HttpHeaders, ContentType, HttpError } from '@tumau/core';
 import { OutgoingHttpHeaders } from 'http';
 
-interface Options {
-  json: object;
+interface Options<T extends object> {
+  json: T;
   code?: HttpStatusCode;
   headers?: OutgoingHttpHeaders;
 }
 
-export class JsonResponse extends TumauResponse {
+export class JsonResponse<T extends object = object> extends TumauResponse {
   public json: object;
 
-  public constructor(options: Options) {
+  public constructor(options: Options<T>) {
     const { code = 200, headers = {}, json } = options;
     const body = JSON.stringify(json);
     const outHeaders: OutgoingHttpHeaders = {
@@ -22,7 +22,7 @@ export class JsonResponse extends TumauResponse {
     this.json = json;
   }
 
-  public static with(json: object): JsonResponse {
+  public static with<T extends object>(json: T): JsonResponse {
     return new JsonResponse({ json });
   }
 
