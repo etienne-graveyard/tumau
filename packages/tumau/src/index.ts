381fcaf67bffbf4140794597c508293448565cb8
Etienne Dldc <e.deladonchamps@gmail.com>
2019-11-06 15:03:04 +0100

    Update tumau

--- packages/tumau/src/index.ts
+++ packages/tumau/src/index.ts
@@ -1,6 +1,8 @@
-export * from '@tumau/core';
-export * from '@tumau/router';
-export * from '@tumau/json';
 export * from '@tumau/compress';
-export * from '@tumau/url-parser';
+export * from '@tumau/core';
 export * from '@tumau/cors';
+export * from '@tumau/json';
+export * from '@tumau/rate-limit';
+export * from '@tumau/router';
+// export * from '@tumau/static';
+export * from '@tumau/url-parser';
