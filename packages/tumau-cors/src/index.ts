070185f5c2d8446c504b8bf3cb9364b56f151479
Etienne Dldc <e.deladonchamps@gmail.com>
2020-02-02 16:09:55 +0100

    Cleanup cors

--- packages/tumau-cors/src/index.ts
+++ packages/tumau-cors/src/index.ts
@@ -1,7 +1,5 @@
-export { CorsContextConsumer } from './CorsContext';
 export { CorsActual, CorsActualConfig } from './CorsActual';
 export { CorsPreflight, CorsPreflightConfig } from './CorsPreflight';
-export { CorsManager } from './CorsManager';
 export { CorsPackage } from './CorsPackage';
 export { CorsActualResponse } from './CorsActualResponse';
 export { CorsPreflightResponse } from './CorsPreflightResponse';
