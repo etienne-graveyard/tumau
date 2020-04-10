42777d2ab371810bd3f216c81ce1d574935af8b6
Etienne Dldc <e.deladonchamps@gmail.com>
2020-04-10 12:11:59 +0200

    Generate Readme

---
+++ scripts/src/generateReadme.ts
@@ -0,0 +1,41 @@
+import * as fse from 'fs-extra';
+import * as path from 'path';
+import { parse, resolve } from 'docsy';
+import * as MDComponents from './utils/MDComponents';
+
+const PROJECT_ROOT = path.resolve(__dirname, '../..');
+const SOURCE_BASE = path.resolve(PROJECT_ROOT, 'docs', 'content');
+
+main();
+
+async function main() {
+  await generateReadme('index.dy', '.');
+}
+
+async function generateReadme(sourcePath: string, destPath: string) {
+  const sourceResolved = path.resolve(SOURCE_BASE, sourcePath);
+  const destResolved = path.resolve(PROJECT_ROOT, destPath, 'README.md');
+  const content = await fse.readFile(sourceResolved, { encoding: 'utf8' });
+  console.log({
+    sourceResolved,
+    destResolved,
+  });
+  const parsed = parse(content);
+  const resolved = resolve(parsed, {
+    createElement: (type: any, props: any, ...children: any) => {
+      try {
+        return type({ ...props, children });
+      } catch (error) {
+        console.log({ ...props, children });
+        throw error;
+      }
+    },
+    ...MDComponents,
+  }).join('');
+  const header = `<!-- This file has been generated, to change it edit ${path.relative(
+    PROJECT_ROOT,
+    sourceResolved
+  )} -->\n`;
+
+  await fse.writeFile(destResolved, header + resolved);
+}
