a108852637d705991f5de79aa10ad8ec8c6a160d
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2022-02-01 11:42:10 +0100

    Fix

--- src/core/Contexts.ts
+++ src/core/Contexts.ts
@@ -3,7 +3,7 @@ import { TumauRequest } from './TumauRequest';
 import { ServerResponse } from 'http';
 import { Duplex } from 'stream';
 
-// We force a deault value because this context is alway present !
+// We force a default value because this context is alway present !
 export const RequestContext = createContext<TumauRequest>({
   name: 'Request',
   defaultValue: null as any,
--- src/core/HttpMethod.ts
+++ src/core/HttpMethod.ts
@@ -1,12 +1,12 @@
 const AllHttpMethod = {
-  GET: 'GET' as const,
-  HEAD: 'HEAD' as const,
-  PATCH: 'PATCH' as const,
-  OPTIONS: 'OPTIONS' as const,
-  DELETE: 'DELETE' as const,
-  POST: 'POST' as const,
-  PUT: 'PUT' as const,
-};
+  GET: 'GET',
+  HEAD: 'HEAD',
+  PATCH: 'PATCH',
+  OPTIONS: 'OPTIONS',
+  DELETE: 'DELETE',
+  POST: 'POST',
+  PUT: 'PUT',
+} as const;
 
 export type HttpMethod = typeof AllHttpMethod[keyof typeof AllHttpMethod];
 
